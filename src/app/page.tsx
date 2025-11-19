6869522d5169eab8559787cffe231f5a86b1cdd1
Esmeralda <st4324@utr.edu.mx>
2025-11-19 13:08:42 -0600

    heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
 import AppRouter from "../routes/AppRouter";
 
 export default function App() {
   return <AppRouter />;
+=======
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen">
+      <HeroSection/>
+    </main>
+  );
+>>>>>>> Stashed changes
 }
